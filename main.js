34d668c8a11ed1449143b95d12a1229d0a53798c
Lee <326656669@qq.com>
2018-01-08 20:13:52 +0800

    完成stickyNavBar,样式小修

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // 给loading一个函数 让别人看得到
 setTimeout(function () {
   siteWelcome.classList.remove('active')
-},1500)
+},0)
 
-
+// stickyNavBar
+window.onscroll = function (e) {
+  window.scrollY > 0 ? topNavBar.classList.add('sticky') : topNavBar.classList.remove('sticky')
+}
 
 portfolio1.onclick= function(){
   portfolioBar.className = 'bar state-1'
