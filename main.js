2afe39677a235e4f918e88ae138c24dcd9202106
Lee <326656669@qq.com>
2018-01-08 23:57:32 +0800

    navTopBar 点击对应 a标签 展示 锚点所示位置

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ setTimeout(function () {
     window.scrollY > 0 ? topNavBar.classList.add('sticky') : topNavBar.classList.remove('sticky')
   }
 },1500)
+
 let liTags = document.querySelectorAll('nav.menu > ul > li')
 for(let i = 0;i<liTags.length;i++) {
   liTags[i].onmouseenter = function (e) {
@@ -15,6 +16,20 @@ for(let i = 0;i<liTags.length;i++) {
     e.currentTarget.classList.remove('active')
   }
 }
+
+let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+for(let i = 0;i<aTags.length;i++){
+  aTags[i].onclick = function(e){
+    e.preventDefault()
+    let a = e.currentTarget
+    let href = a.getAttribute('href')
+    // a.href 是浏览器解析后的带HTTP的地址,不是我想要的地址 这里得到的结果是 '#siteAbout'
+    let element = document.querySelector(href)
+    let top = element.offsetTop  // 获取对应锚点距离浏览器高度是多少
+    window.scrollTo(0,top - 80)
+  }
+}
+
 
 portfolio1.onclick= function(){
   portfolioBar.className = 'bar state-1'
