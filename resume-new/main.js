b15e9dece0712625ab715133aa8df2730fe03a1a
Lee <326656669@qq.com>
2018-01-31 20:44:19 +0800

    修改一个BUG 添加swiper插件

--- resume-new/main.js
+++ resume-new/main.js
@@ -3,43 +3,46 @@ siteWelcome.classList.remove('active')
 
 // 添加 offset 类
 let specialTags = document.querySelectorAll('[data-x]')
-for(let i =0;i<specialTags.length; i++){
+for (let i = 0; i < specialTags.length; i++) {
   specialTags[i].classList.add('offset')
 }
-setTimeout(function(){
+setTimeout(function () {
   findClosest()
-},200)
+}, 200)
 // stickyNavBar
 window.onscroll = function () {
   window.scrollY > 0 ? topNavBar.classList.add('sticky') : topNavBar.classList.remove('sticky')
   findClosest()
 }
 
-function findClosest(){
+function findClosest () {
   // 滚动到对应锚点 二级菜单对应标签高亮
   let specialTags = document.querySelectorAll('[data-x]')
   let minIndex = 0
-  let id = specialTags[minIndex].id
-  let a = document.querySelector(`a[href="#${id}"]`)
-  let li = a.parentNode
-  let siblings =  li.parentNode.children
-  // 找到最近的类
-  for(let i = 0;i<specialTags.length;i++){
-    if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+  for (let i = 1; i < specialTags.length; i++) {
+    if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
       minIndex = i
     }
   }
   // 去除默认offset类
   specialTags[minIndex].classList.remove('offset')
+
+  let id = specialTags[minIndex].id
+  let a = document.querySelector(`a[href="#${id}"]`)
+  let li = a.parentNode
+  let siblings = li.parentNode.children
+  // 找到最近的类
+
   // 给二级菜单添加highlight类
-  for(let i = 0 ;i<siblings.length;i++){
+  for (let i = 0; i < siblings.length; i++) {
     siblings[i].classList.remove('highlight')
   }
   li.classList.add('highlight')
 }
+
 // 二级菜单展示
 let liTags = document.querySelectorAll('nav.menu > ul > li')
-for(let i = 0;i<liTags.length;i++) {
+for (let i = 0; i < liTags.length; i++) {
   liTags[i].onmouseenter = function (e) {
     e.currentTarget.classList.add('active')
   }
@@ -49,16 +52,16 @@ for(let i = 0;i<liTags.length;i++) {
 }
 
 // 二级菜单点击标签 跳转对应锚点
-function animate(time) {
-  requestAnimationFrame(animate);
-  TWEEN.update(time);
+function animate (time) {
+  requestAnimationFrame(animate)
+  TWEEN.update(time)
 }
-requestAnimationFrame(animate);
 
+requestAnimationFrame(animate)
 
 let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-for(let i = 0;i<aTags.length;i++){
-  aTags[i].onclick = function(e){
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function (e) {
     e.preventDefault()
     // let a = e.currentTarget
     // let href = a.getAttribute('href')
@@ -71,26 +74,25 @@ for(let i = 0;i<aTags.length;i++){
     let currentTop = window.scrollY
     let targetTop = top - 80
     let s = targetTop - currentTop
-    let t = Math.abs((s/100)*300)
-    if(t>500){t=500}
-    let coords = { y: currentTop};
+    let t = Math.abs((s / 100) * 300)
+    if (t > 500) {t = 500}
+    let coords = {y: currentTop}
     let tween = new TWEEN.Tween(coords)
-      .to({ y: targetTop}, t)
+      .to({y: targetTop}, t)
       .easing(TWEEN.Easing.Quadratic.InOut)
-      .onUpdate(function() {
-        window.scrollTo(0,coords.y)
+      .onUpdate(function () {
+        window.scrollTo(0, coords.y)
       })
-      .start();
+      .start()
   }
 }
 
-
-portfolio1.onclick= function(){
+portfolio1.onclick = function () {
   portfolioBar.className = 'bar state-1'
 }
-portfolio2.onclick= function(){
+portfolio2.onclick = function () {
   portfolioBar.className = 'bar state-2'
 }
-portfolio3.onclick= function(){
-    portfolioBar.className = 'bar state-3'
-  }
+portfolio3.onclick = function () {
+  portfolioBar.className = 'bar state-3'
+}
