434c7a6c3003de81ed8ab227fdcf4d2fd0f8ed02
Lee <326656669@qq.com>
2018-02-02 23:35:56 +0800

    优化MVC 添加model

--- resume-new/js/message.js
+++ resume-new/js/message.js
@@ -1,40 +1,47 @@
 !function () {
   let view = document.querySelector('section.message')
-  let controller = {
-    view: null,
-    messageList: null,
-    init: function (view) {
-      this.view = view
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('#postMessage')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    initAV: () => {
+  let model = {
+    init: function () {
       let APP_ID = 'JLMh9Uyk8yngdHl7RjiKtBvJ-gzGzoHsz'
       let APP_KEY = 'eFTOCJrbFsKTdigcxbv3jRog'
       AV.init({appId: APP_ID, appKey: APP_KEY})
     },
-    loadMessages: () => {
+    fetch: function () {
       let query = new AV.Query('Message')
-      query.find()
-        .then(
-          (messages) => {
-            let array = messages.map((item) => item.attributes)
-            array.forEach((item) => {
-              let li = document.createElement('li')
-              li.innerText = `${item.user} : ${item.content}`
-              this.messageList.append(li)
-            })
-          },
-          (error) => {
-            alert('提交失败')
-            // 异常处理
+      return query.find()
+    },
+    save: function (user, content) {
+      let Message = AV.Object.extend('Message')
+      let message = new Message()
+      return message.save({user, content})
+    }
+  }
+  let controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('#postMessage')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.user} : ${item.content}`
+            this.messageList.append(li)
           })
+        })
     },
     bindEvents: function () {
-      this.form.addEventListener('submit', (e)=>{  // 监听表单防止 监听submit落空
+      this.form.addEventListener('submit', (e) => {  // 监听表单防止 监听submit落空
         e.preventDefault()
         this.saveMessage()
       })
@@ -43,23 +50,17 @@
       let myForm = this.form
       let user = myForm.querySelector('#messageUser').value
       let content = myForm.querySelector('#messageContent').value
-      let Message = AV.Object.extend('Message')
-      let message = new Message()
-      message.save({
-        user,
-        content
-      })
-        .then(
-          function (object) {
-            let li = document.createElement('li')
-            li.innerText = `${object.attributes.user}: ${object.attributes.content}`
-            let messageList = document.querySelector('#messageList')
-            messageList.append(li)
-            myForm.querySelector('#messageUser').value = ''
-            myForm.querySelector('#messageContent').value = ''
-          }
-        )
+      this.model.save(user, content).then(
+        function (object) {
+          let li = document.createElement('li')
+          li.innerText = `${object.attributes.user}: ${object.attributes.content}`
+          let messageList = document.querySelector('#messageList')
+          messageList.append(li)
+          myForm.querySelector('#messageUser').value = ''
+          myForm.querySelector('#messageContent').value = ''
+        }
+      )
     }
   }
-  controller.init(view)
+  controller.init(view, model)
 }.call()
