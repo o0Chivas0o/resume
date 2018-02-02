13788ac677f46708ff1f6f3a802c82615e5a21b6
Lee <326656669@qq.com>
2018-02-02 23:16:27 +0800

    1.修改为MVC 2.html中label与input互相绑定

--- resume-new/js/message.js
+++ resume-new/js/message.js
@@ -1,46 +1,65 @@
-let APP_ID = 'JLMh9Uyk8yngdHl7RjiKtBvJ-gzGzoHsz'
-let APP_KEY = 'eFTOCJrbFsKTdigcxbv3jRog'
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-})
-
-let myFrom = document.querySelector('#postMessage')
-
-myFrom.addEventListener('submit', function (e) {  // 监听表单防止 监听submit落空
-  e.preventDefault()
-  let user = document.querySelector('input[name=user]').value
-  let content = document.querySelector('input[name=content]').value
-  let Message = AV.Object.extend('Message')
-  let message = new Message()
-  message.save({
-    user,
-    content
-  }).then(function (object) {
-    let li = document.createElement('li')
-    li.innerText = `${object.attributes.user}: ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.append(li)
-    document.querySelector('input[name=user]').value = ''
-    document.querySelector('input[name=content]').value = ''
-  })
-})
-
-let query = new AV.Query('Message')
-query.find()
-  .then(
-    (messages) => {
-      let array = messages.map((item) => item.attributes)
-      array.forEach((item) => {
-        let li = document.createElement('li')
-        li.innerText = `${item.user} : ${item.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.append(li)
+!function () {
+  let view = document.querySelector('section.message')
+  let controller = {
+    view: null,
+    messageList: null,
+    init: function (view) {
+      this.view = view
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('#postMessage')
+      this.initAV()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    initAV: () => {
+      let APP_ID = 'JLMh9Uyk8yngdHl7RjiKtBvJ-gzGzoHsz'
+      let APP_KEY = 'eFTOCJrbFsKTdigcxbv3jRog'
+      AV.init({appId: APP_ID, appKey: APP_KEY})
+    },
+    loadMessages: () => {
+      let query = new AV.Query('Message')
+      query.find()
+        .then(
+          (messages) => {
+            let array = messages.map((item) => item.attributes)
+            array.forEach((item) => {
+              let li = document.createElement('li')
+              li.innerText = `${item.user} : ${item.content}`
+              this.messageList.append(li)
+            })
+          },
+          (error) => {
+            alert('提交失败')
+            // 异常处理
+          })
+    },
+    bindEvents: function () {
+      this.form.addEventListener('submit', (e)=>{  // 监听表单防止 监听submit落空
+        e.preventDefault()
+        this.saveMessage()
       })
     },
-    (error) => {
-      alert('提交失败')
-      // 异常处理
-    })
-
+    saveMessage: function () {
+      let myForm = this.form
+      let user = myForm.querySelector('#messageUser').value
+      let content = myForm.querySelector('#messageContent').value
+      let Message = AV.Object.extend('Message')
+      let message = new Message()
+      message.save({
+        user,
+        content
+      })
+        .then(
+          function (object) {
+            let li = document.createElement('li')
+            li.innerText = `${object.attributes.user}: ${object.attributes.content}`
+            let messageList = document.querySelector('#messageList')
+            messageList.append(li)
+            myForm.querySelector('#messageUser').value = ''
+            myForm.querySelector('#messageContent').value = ''
+          }
+        )
+    }
+  }
+  controller.init(view)
+}.call()
